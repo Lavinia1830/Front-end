da12f84258cdcf84292d8f4b430489710b379342
Lavinia1830 <lavinia.baratti@gmail.com>
2023-11-12 16:24:19 +0100

    Esercizio giono 12 - JavaScript

--- esercizio giorno 12 - JavaScript/js/script.js
+++ esercizio giorno 12 - JavaScript/js/script.js
@@ -1,33 +1,37 @@
 
-function tabella (){
-    let tabella = document.querySelector('#numberTombola'); 
-    /*let tabellaNumeri = document.createElement('table'); */
-    for(let i=1; i<=76; i++){
-        let cella = document.createElement('div');
-        cella.innerHTML = [i];
-        tabella.appendChild(cella);
-        cella.className = 'cellaNumber';
+function creaTabellone() {
+    let tabella = document.getElementById('tabellone-body');
+    let numeroCella = 1;
+
+    for (let riga = 1; riga <= 4; riga++) {
+      let tr = document.createElement('tr');
+
+      for (let colonna = 1; colonna <= 19; colonna++) {
+        let td = document.createElement('td');
+        td.textContent = numeroCella;
+        tr.appendChild(td);
+        numeroCella++;
+      }
+        tabella.appendChild(tr);
     }
 }
-tabella();
+creaTabellone();
 
 
 function numeroRandom(){
     let btn = document.querySelector('#button');
     btn.addEventListener("click", () => {
         let n = Math.floor((Math.random() * 75) + 1);
-        
         console.log(n);
-    })
-    
+    }) 
 }
 numeroRandom();
 
-let cella = document.querySelectorAll('#cellaNumero');
+/* let cella = document.querySelectorAll('#cellaNumero');
 function colorCella(){
      let numberRandom = numeroRandom();
     if(cella === numberRandom){
         cella.className = 'colorCella';
     }
-}
+} */
 
