9334ed0f271fb6479e6db3e69b006f80294eed84
Lavinia1830 <lavinia.baratti@gmail.com>
2023-12-09 16:21:37 +0100

    Aggiornamento

--- Esercizio giorno 23 - JavaScript/js/script.js
+++ Esercizio giorno 23 - JavaScript/js/script.js
@@ -13,38 +13,23 @@ xhr.onreadystatechange = function(){
         
         obj.forEach(e => { //quando dobbiamo scrivere i comadi in HTML
             let card = document.querySelector('#books-wrapper');
-
-<<<<<<< HEAD
-            card.innerHTML +=
+            
+        card.innerHTML +=
             `<div class="col">
                 <div class="card shadow-sm h-100">
                     <img src="${e.img}" class="card-img-top" alt="${e.title}">
                     <div class="card-body">
-=======
-            card.innerHTML =
-            `
-            <div class="card m-2 my-5" style="width: 18rem;">
-                <img src="${e.img}" class="card-img-top" alt="Immagine dei libri">
                 <div class="card-body">
+                    <h5 class="card-title">${e.title}</h5>
+                    <p class="card-text badge rounded-pill text-bg-dark">${e.category}</p>
+                    <p class="fs-4">${e.price}</p> <!--fs: font-size = misura del font--> 
                     <div>
->>>>>>> dd885aadc643a9eda309a1dad8aa27259deaf52d
-                        <h5 class="card-title">${e.title}</h5>
-                        <p class="card-text badge rounded-pill text-bg-dark">${e.category}</p>
-                        <p class="fs-4">${e.price}</p> <!--fs: font-size = misura del font--> 
-                        <div>
-                            <a href="#" class="btn btn-primary">Compra ora</a>
-                            <a class="btn btn-outline-danger" onclick="skip(event)">Scarta</a>
-                        </div>
+                        <a href="#" class="btn btn-primary">Compra ora</a>
+                        <a class="btn btn-outline-danger" onclick="skip(event)">Scarta</a>
                     </div>
                 </div>
             </div>`
-            
         });
-<<<<<<< HEAD
-
-        
-=======
->>>>>>> dd885aadc643a9eda309a1dad8aa27259deaf52d
     }
     
 }
