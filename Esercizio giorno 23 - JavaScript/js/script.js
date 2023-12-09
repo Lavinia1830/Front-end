22d9b450bc7f470fd4a07fca748037304f124857
Lavinia1830 <lavinia.baratti@gmail.com>
2023-12-09 16:14:03 +0100

    Aggiornamento

--- Esercizio giorno 23 - JavaScript/js/script.js
+++ Esercizio giorno 23 - JavaScript/js/script.js
@@ -12,68 +12,34 @@ xhr.onreadystatechange = function(){
         //console.log(obj);
         
         obj.forEach(e => { //quando dobbiamo scrivere i comadi in HTML
-            let div = document.querySelector('#containerCard');
-            let card = document.createElement('div');
-            div.appendChild(card);
+            let card = document.querySelector('#books-wrapper');
 
-            card.innerHTML =
-            `<div class="card m-2 my-5 col" style="width: 18rem;">
-                <img src="${e.img}" class="card-img-top" alt="Immagine dei libri">
-                <div class="card-body ">
-                    <div>
+            card.innerHTML +=
+            `<div class="col">
+                <div class="card shadow-sm h-100">
+                    <img src="${e.img}" class="card-img-top" alt="${e.title}">
+                    <div class="card-body">
                         <h5 class="card-title">${e.title}</h5>
-                        <span class="badge rounded-pill text-bg-dark">${e.category}</span>
-                    </div>
-                    <div>
-                        <p class="card-text">${e.price}</p>
-                        <a href="#" class="btn btn-primary">Compra ora</a>
-                        <a href="#" class="btn btn-danger">Scarta</a>
+                        <p class="card-text badge rounded-pill text-bg-dark">${e.category}</p>
+                        <p class="fs-4">${e.price}</p> <!--fs: font-size = misura del font--> 
+                        <div>
+                            <a href="#" class="btn btn-primary">Compra ora</a>
+                            <a class="btn btn-outline-danger" onclick="skip(event)">Scarta</a>
+                        </div>
                     </div>
                 </div>
             </div>`
+            
         });
 
-
-        /* for(let i = 0; i < 50; i++){
-            //console.log(obj[i].img);
-            let div = document.querySelector('.cardImage');
-            let image = document.querySelector('img');
-            let divBody = document.querySelector('.card-body');
-            let title = document.querySelector('h5');
-            image.src = obj[i].img;
-            title = obj[i].title;
-            //console.log(obj[i].title);
-            div.appendChild(image);
-            divBody.appendChild(title);
-            
-            
-        }
-        for (let i = 0; i < obj.length; i++){
-            
-            
-            
-        } */
-        /* for(let i = 0; i < obj.length; i++){
-            //console.log(obj[i].img);
-            let card = document.querySelector('.card');
-            let div = document.createElement('div');
-            let divBody = document.createElement('div');
-            div.classList.add("cardImage");
-            divBody.classList.add("cardBody");
-            let image = document.createElement('img');
-            let title = document.createElement('p');
-            let price = document.createElement('p');
-            image.src = obj[i].img;
-            title = obj[i].title;
-            price = obj[i].price;
-            card.appendChild(div);
-            div.appendChild(image);
-            divBody.appendChild(title);
-
-        } */
+        
     }
     
 }
-
+const skip = (event) => { /*Questa funzione con il 
+    parametro evento quando fai click sull'a che 
+    abbiamo trasformato in un pulsante scarta lui lo rimuove dalla lista*/
+    event.target.closest('.col').remove() 
+}
 
 
